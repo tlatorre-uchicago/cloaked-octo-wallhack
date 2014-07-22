126d2a4d2b0169570ae08143f26ac198153166e6
Anthony LaTorre <tlatorre@uchicago.edu>
2014-07-22 09:48:30 -0500

    update foo

--- foo.py
+++ foo.py
@@ -1,13 +1,30 @@
+from __future__ import print_function
 from zmq_client import V1729Client
 
-client = V1729Client('hollande.uchicago.edu',5555)
-client.set_trigger_source(1)
-client.set_trigger_type(3,'falling')
-client.set_trigger_threshold(-20e-3) # -20 mV
+digi = V1729Client('hollande.uchicago.edu',5555)
+digi.set_trigger_source(1)
+digi.set_trigger_type(3,'falling')
+digi.set_trigger_threshold(-10e-3) # -10 mV
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+    import sys
+    import h5py
+    import argparse
 
-    data = client.get_data()
-    plt.plot(data)
-    plt.show()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', dest='filename', default='output.hdf5')
+    parser.add_argument('-n', dest='N', default=1000, type=int,
+                        help='number of events')
+    args = parser.parse_args()
+
+    f = h5py.File(args.filename,'w')
+    for i in range(1,5):
+        dset = f.create_dataset('c%i' % i, (args.N, 2520), dtype='i')
+
+    for i in range(args.N):
+        print('\r%i/%i' % (i+1,args.N),end='')
+        sys.stdout.flush()
+        data = digi.get_data()
+        for j, v in enumerate(data.T):
+            f['c%i' % (j+1)][i] = v
+    print()
